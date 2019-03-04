dd92a045f580b380df7d280044917b951c68e538
Daniel Morais <damorais@gmail.com>
2019-03-04 15:25:55 -0300

    Source cleanup and new README

--- src/Client/Client.fs
+++ src/Client/Client.fs
@@ -9,10 +9,6 @@ module Client
 
     open Fable.Helpers.React
 
-    // open Fulma
-
-    // type Model = { Counter: Counter }
-
     type Model = {
         Document : Document
         CurrentLine : int
@@ -26,18 +22,10 @@ module Client
         CurrentLine = 1
     }
 
-    // type Msg =
-    // | Increment
-    // | Decrement
-    // | UpdateValue of int
-
     type Msg =
     | NewLine of string
     | RemoveLine of int
     | UpdateLine of string * int
-
-    // let init () : Model * Cmd<Msg> =
-    //     { Counter = { Value = 0 } } , Cmd.none
 
     let init () : Model * Cmd<Msg> =
         newModel, Cmd.none
@@ -49,34 +37,6 @@ module Client
             nextModel, Cmd.none
         | currentModel, RemoveLine index -> currentModel, Cmd.none
         | currentModel, UpdateLine (content, index) -> currentModel, Cmd.none
-
-    // let update (msg : Msg) (currentModel : Model) : Model * Cmd<Msg> =
-    //     match currentModel.Counter, msg with
-    //     | counter, Increment ->
-    //         let nextModel = { currentModel with Counter = { Value = counter.Value + 1 } }
-    //         nextModel, Cmd.none
-    //     | counter, Decrement ->
-    //         let nextModel = { currentModel with Counter = { Value = counter.Value - 1 } }
-    //         nextModel, Cmd.none
-    //     | _ , UpdateValue value ->
-    //         let nextModel = { currentModel with Counter = { Value = value }}
-    //         nextModel, Cmd.none
-
-
-    // let view (model : Model) (dispatch : Msg -> unit ) =
-    //     div []
-    //         [
-    //             str "Digite um número: "
-    //             button [ OnClick (fun _ -> dispatch Decrement)] [ str "-" ]
-    //             input [
-    //                     Id "txtContador"
-    //                     Type "text"
-    //                    OnChangealue model.Counter.Value
-    //                    OnChangenChange (fun e -> dispatch (UpdateValue (int e.Value)))
-    //                   ]OnChange
-    //             button [ OnClick (fun _ -> dispatch Increment)] [ str "+" ]
-    //             str ("O valor atual é: " + (string model.Counter.Value ))
-    //         ]
 
 
     let documentLine (line:ContentLine) =
